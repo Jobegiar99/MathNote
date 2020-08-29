b430ee2e35967866b6ed10675b0e7cc5a20db338
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-29 12:10:09 -0600

    Added folders

--- src/Components/Js/index.js
+++ src/Components/Js/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component{
-
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
 }
 ReactDOM.render(
   <React.StrictMode>
