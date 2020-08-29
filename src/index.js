198467991d4596dcb868dc4176dace0b2ba6cb5e
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-29 12:56:34 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import {playAudio} from "./Functions/audiotest.js";
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        
+        <button onClick = {playAudio}>PLAY</button>
       </div>
     )
   }
