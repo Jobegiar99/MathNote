f845718b329efc034cbe38838ad9b07327b60dd6
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-29 22:29:30 -0600

    minor fix

--- src/Functions/playNotes.js
+++ src/Functions/playNotes.js
@@ -1,36 +1,52 @@
 import {piano} from "./pianoDictionary";
 import { Howl, Howler } from 'howler';
-var delay = require('delay');
+import { wait } from "@testing-library/react";
+const delay = require('delay');
 
-export const GetInput = async(info, play,speed) =>{
-    
-    let valuesToGraph = []
+export const  GetInput = async(info,updateGraph,speed) =>{
     let i = 0, j = 4;
-    while( i < info.lenth){
+    while( i < info[0].length){
 
-        for(let l = i; l < j && l < info.lenth; l++){
+        let valuesToGraph = [];
+
+        //add values that will be sent to graph
+        for(let l = i; l <= j && l < info.length;l++){
 
             let temp = [];
 
-            for( let k = l; k < info[l].lenth; k++){
-                temp.push(info[l][k]);
+            for(let k = l; k < info[l].length;k++){
+                temp.push(info[l][k])
             }
 
             valuesToGraph.push(temp);
 
         }
+        console.log(valuesToGraph);
 
+        //play notes
         let note = [];
-        for(let k = 0; k < info.lenth;k++){ 
-            note.forEach( x => {
-                let toPlay = new Howl({
-                    src : [x],
-                    volume: 1
-                });
-                toPlay.toPlay();
-            })
+        for(let  k = 0; k < info.length; k++){
+            if( i < info[k].length){
+                note[k] = (piano[ info[k][i] ]);
+            }
+
         }
+        console.log(note);
+        if(note.length > 0){
+            note.forEach(x => {
+
+            let toPlay= new Howl({
+                    src: [x],
+                    volume : 1
+            }); 
+            toPlay.play();
+            });
+
+        }
+
+        //updateGraph(valuesToGraph,i,j);
         await delay(speed);
+
         i++;
         j++;
     }
