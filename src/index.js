731f11a98b0c330d1e5c5895f8b8c6134c0b2d41
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-29 20:30:21 -0600

    Function to play and update graph done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {GetInput} from "./Functions/playNotes";
 class App extends React.Component{
 
-  render(){    
+  help = () =>{
+    let nums = [
+      [0,1,2,3,4,5,null,0,0,0],
+      [10,10,10,10,0]
+    ] ; 
+    GetInput(nums, null,1000);
+  }
+
+  render(){  
+    
     return(
       <div>
+        <button onClick = {this.help}>PLAY</button>
       </div>
     )
   }
