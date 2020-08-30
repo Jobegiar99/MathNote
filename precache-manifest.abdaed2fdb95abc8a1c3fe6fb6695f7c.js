c3b675c8e6cda4f7f9c03eebfad30080f4076789
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-30 14:10:47 -0600

    Updates

---
+++ precache-manifest.abdaed2fdb95abc8a1c3fe6fb6695f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cba8291d9c106d595745a1b0f9ca90",
+    "url": "/mathnote.github.io/index.html"
+  },
+  {
+    "revision": "37d1a8d5b0bedaa5a8fa",
+    "url": "/mathnote.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a3d3c7e72336ef396e07",
+    "url": "/mathnote.github.io/static/css/main.1bc8452a.chunk.css"
+  },
+  {
+    "revision": "37d1a8d5b0bedaa5a8fa",
+    "url": "/mathnote.github.io/static/js/2.82411981.chunk.js"
+  },
+  {
+    "revision": "0521c0b3e20a55225fc7a880fd2a9704",
+    "url": "/mathnote.github.io/static/js/2.82411981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d3c7e72336ef396e07",
+    "url": "/mathnote.github.io/static/js/main.fb3f3732.chunk.js"
+  },
+  {
+    "revision": "7524acae2d4725f27c2a",
+    "url": "/mathnote.github.io/static/js/runtime-main.7355f4c0.js"
+  }
+]);
